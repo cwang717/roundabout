a3f16321702243775539cc6b04ae69f4da60331d
Chaojie Wang <cwang717@gatech.edu>
2021-07-10 17:36:34 -0400

    change test to main

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
+from roundabout.utils import test_plot
+from roundabout.utils import test_ani, delta_plot
+from roundabout.env import Env
 import numpy as np
+from roundabout.utils import test_ani
+import matplotlib
+matplotlib.use('Agg')
+np.seterr(all='raise')
 
-envParams = {"a": 3, 
-             "v": 8,
-             "C": 3,
-             "veh_length": 5,
-             "headway": 3}
-
-initParams = {"l_0": np.array([4, 5, 3, 4]),
-              "Q": np.array([18, 25, 20, 20])  
+envParams = {
+    "a": 3, 
+    "v": 8,
+    "C": 3,
+    "veh_length": 5,
+    "headway": 3,
+    "Q": np.array([30, 25, 35, 20]),
+    "step_size": 0.05,
+    "boundary": 250,
+}
+initParams = {
+    #"l_0": np.array([10, 0, 0, 0]),  
+    "l_0": np.array([14, 15, 13, 14]),  
 }
 
-V0 = np.ones(4)*v      # the average speeds of vehicles generated on each approaches
-V0_std = np.ones(4)    # std of the speeds of vehicles generated on each approaches
+env = Env(envParams)
+env.initialize(initParams)
 
+
+
+test_ani(env, 6000)
